12e1c1618e0195e7b5625a16a377c0b65ad54ba5
Aditya Dhananjay <aditya.dhananjay@gmail.com>
2021-02-23 13:40:31 -0500

    Windows and Linux EOF missing weirdness fixed

--- sdcard/autostart.sh
+++ sdcard/autostart.sh
@@ -3,8 +3,11 @@ ifconfig -a | grep eth0
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
     ifconfig eth0 down
-    ifconfig eth0 192.168.1.43 up
-	ifconfig eth0 192.168.1.43 netmask 255.255.255.0
+    ifconfig eth0 10.1.1.43 up
+    ifconfig eth0 down
+    ifconfig eth0 10.1.1.43 up
+	ifconfig eth0 10.1.1.43 netmask 255.255.255.0
 	piradio&
 	rftool
 fi
+echo "Done!"
